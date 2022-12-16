b2cb8d0990463640f62d7bfb5934f9035bc3ca7c
Maksim Vladimirov <vl4dimirov@gmail.com>
2022-12-16 16:07:20 +0300

    add calc form and filter

---
+++ src/ts/modules/calc.ts
@@ -0,0 +1,41 @@
+export const calc = ({
+  size,
+  material,
+  options,
+  promocode,
+  result,
+}: {
+  size: string;
+  material: string;
+  options: string;
+  promocode: string;
+  result: any;
+}) => {
+  const sizeBlock = document.querySelector<HTMLInputElement>(size);
+  const materialBlock = document.querySelector<HTMLInputElement>(material);
+  const optionsBlock = document.querySelector<HTMLInputElement>(options);
+  const promocodeBlock = document.querySelector<HTMLInputElement>(promocode);
+  const resultBlock = document.querySelector(result);
+
+  let sum = 0;
+
+  const calcFunc = () => {
+    sum = Math.round(
+      +sizeBlock.value * +materialBlock.value + +optionsBlock.value
+    );
+
+    if (sizeBlock.value == "" || materialBlock.value == "") {
+      resultBlock.textContent =
+        "Пожалуйста, выберите размер и материал картины";
+    } else if (promocodeBlock.value === "IWANTPOPART") {
+      resultBlock.textContent = Math.round(sum * 0.7);
+    } else {
+      resultBlock.textContent = sum;
+    }
+  };
+
+  sizeBlock.addEventListener("change", calcFunc);
+  materialBlock.addEventListener("change", calcFunc);
+  optionsBlock.addEventListener("change", calcFunc);
+  promocodeBlock.addEventListener("input", calcFunc);
+};
--- src/ts/modules/checkTextInputs.ts
+++ src/ts/modules/checkTextInputs.ts
@@ -1,12 +1,11 @@
-export const checkTextInput = (selector: string) => { 
-const txtInputs = document.querySelectorAll(selector)
+export const checkTextInput = (selector: string) => {
+  const txtInputs = document.querySelectorAll(selector);
 
-txtInputs.forEach((input: HTMLElement) => {
-    input.addEventListener('keypress', function(e: any ) {
-        if (e.key.match(/[^а-яё 0-9]/ig)) {
-            e.preventDefault();
-        }
-    })
-})
-
-}
+  txtInputs.forEach((input: HTMLElement) => {
+    input.addEventListener("keypress", function (e: any) {
+      if (e.key.match(/[^а-яё 0-9]/gi)) {
+        e.preventDefault();
+      }
+    });
+  });
+};
